a7e31700bbed274c62aa925b2d0b43e4439414ec
Tim Riggs <thriggs@gmail.com>
2023-10-20 02:35:12 +0000

    backup check log should remove old lock file

--- mysql-backup-lock-age-check.sh
+++ mysql-backup-lock-age-check.sh
@@ -4,4 +4,5 @@ LOCK_FILE='/root/mysql-backup.lock'
 if test `find "$LOCK_FILE" -mmin +120`
 then
     echo "$LOCK_FILE" is older than two hours. MySQL Dump may have failed.
+    rm -f $LOCK_FILE;
 fi
